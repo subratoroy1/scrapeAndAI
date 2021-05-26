d7c367ccb3c8f593a4cd12cc2d4b22fa7fe156a1
subrato roy <roy.subrato@gmail.com>
2021-05-26 17:24:25 +0000

    .

--- rrightMove.py
+++ rrightMove.py
@@ -15,17 +15,10 @@ class QuotesSpider(CrawlSpider):
     def parse_aPropertyURL(self,response):
             print(" i am inside a property page")
             #inspect_response(response,self)
-            yield {
-                num_fields : len(response.css('div[id="root"]').css('div[class="tmJOVKTrHAB4bLpcMjzQ"]::text').getall())
-                # a loop here to get label and values
-                'labeli' : response.css('div[id="root"]').css('div[class="tmJOVKTrHAB4bLpcMjzQ"]::text')[i].root,
-                'valuei': response.css('div[id="root"]').css('div[class="_1fcftXUEbWfJOJzIUeIHKt"]::text')[1].root,
-
-                'test': response.xpath('//*[@id="root"]/div/div[4]/main/div[5]/div[1]/div[2]/div[2]/div/text()').get(),
-                
-                'size': response.xpath('//*[@id="root"]/div/div[4]/main/div[5]/div[4]/div[2]/div[2]/div[1]').get(),
-                'short_description': response.xpath('//*[@id="root"]/div/div[4]/main/div[9]/div/div').get()
-            }
+            numLabels = len(response.css('div[id="root"]').css('div[class="tmJOVKTrHAB4bLpcMjzQ"]::text').getall())
+            for i in range(numLabels):
+                yield {response.css('div[id="root"]').css('div[class="tmJOVKTrHAB4bLpcMjzQ"]::text')[i].root : 
+                        response.css('div[id="root"]').css('div[class="_1fcftXUEbWfJOJzIUeIHKt"]::text')[i].root}
 	
 process = CrawlerProcess()
 process.crawl(QuotesSpider)
