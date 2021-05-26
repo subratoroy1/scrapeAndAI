7bd42a7ac07600bbba78a99917d2a4ee51382824
subrato roy <roy.subrato@gmail.com>
2021-05-26 18:12:40 +0000

    .

--- rrightMove.py
+++ rrightMove.py
@@ -4,7 +4,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.crawler import CrawlerProcess 
 from scrapy.spiders import Rule,CrawlSpider
 
-class QuotesSpider(CrawlSpider):
+class RightMoveSpider(CrawlSpider):
     #scrapy.Spider):
     name = 'property data'
     start_urls = [
@@ -15,11 +15,15 @@ class QuotesSpider(CrawlSpider):
     def parse_aPropertyURL(self,response):
             print(" i am inside a property page")
             #inspect_response(response,self)
+            aProperty = {}
+            aProperty['url'] = response.url
             numLabels = len(response.css('div[id="root"]').css('div[class="tmJOVKTrHAB4bLpcMjzQ"]::text').getall())
             for i in range(numLabels):
-                yield {response.css('div[id="root"]').css('div[class="tmJOVKTrHAB4bLpcMjzQ"]::text')[i].root : 
-                        response.css('div[id="root"]').css('div[class="_1fcftXUEbWfJOJzIUeIHKt"]::text')[i].root}
-	
-process = CrawlerProcess()
-process.crawl(QuotesSpider)
-process.start()
+                aProperty[response.css('div[id="root"]').css('div[class="tmJOVKTrHAB4bLpcMjzQ"]::text')[i].root] = response.css('div[id="root"]').css('div[class="_1fcftXUEbWfJOJzIUeIHKt"]::text')[i].root
+            aProperty['price'] = response.css('div[id="root"]').css('div[class="_1gfnqJ3Vtd1z40MlC0MzXu"]').xpath('span')[0].root.text
+            yield aProperty
+
+# enable below to be able to debug as a standalone python script	
+#process = CrawlerProcess()
+#process.crawl(RightMoveSpider)
+#process.start()
